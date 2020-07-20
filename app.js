b8371ead8c61f0c175214dfd709955090cc19915
Ian Chou <ianchou19@gmail.com>
2020-07-20 19:44:17 -0400

    JavaScript Aside: The XMLHTTPRequest Object

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
-    
+myApp.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
+
     $scope.handle = '';
-    
-    $scope.lowercasehandle = function() {
+
+    $scope.lowercasehandle = function () {
         return $filter('lowercase')($scope.handle);
     };
-    
+
     $scope.characters = 5;
-    
-    $scope.rules = [
-      
-        { rulename: "Must be 5 characters" },
-        { rulename: "Must not be used elsewhere" },
-        { rulename: "Must be cool" }
-        
-    ];
-    
-    console.log($scope.rules);
-    
+
+    var rulesrequest = new XMLHttpRequest();
+    rulesrequest.onreadystatechange = function () {
+        $scope.$apply(function () {
+            if (rulesrequest.readyState == 4 && rulesrequest.status == 200) {
+                $scope.rules = JSON.parse(rulesrequest.responseText);
+            }
+        });
+    }
+    rulesrequest.open("GET", "http://localhost:54765/api", true);
+    rulesrequest.send();
+
 }]);
