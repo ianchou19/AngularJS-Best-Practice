788fca2a04ae76cf9e6740f1adbec546f9827bd5
Ian Chou <ianchou19@gmail.com>
2020-07-20 21:14:46 -0400

    External Data and

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', '$filter', function ($scope, $filter) {
+myApp.controller('mainController', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
 
     $scope.handle = '';
 
@@ -10,15 +10,32 @@ myApp.controller('mainController', ['$scope', '$filter', function ($scope, $filt
 
     $scope.characters = 5;
 
-    var rulesrequest = new XMLHttpRequest();
-    rulesrequest.onreadystatechange = function () {
-        $scope.$apply(function () {
-            if (rulesrequest.readyState == 4 && rulesrequest.status == 200) {
-                $scope.rules = JSON.parse(rulesrequest.responseText);
-            }
-        });
-    }
-    rulesrequest.open("GET", "http://localhost:54765/api", true);
-    rulesrequest.send();
+    $http.get('/api')
+        .success(function (result) {
 
+            $scope.rules = result;
+
+        })
+        .error(function (data, status) {
+
+            console.log(data);
+
+        });
+
+    $scope.newRule = '';
+    $scope.addRule = function () {
+        $http.post('/api', { newRule: $scope.newRule })
+            .success(function (result) {
+
+                console.log(result);
+                $scope.rules = result;
+                $scope.newRule = '';
+
+            })
+            .error(function (data, status) {
+
+                console.log(data);
+
+            });
+    };
 }]);
